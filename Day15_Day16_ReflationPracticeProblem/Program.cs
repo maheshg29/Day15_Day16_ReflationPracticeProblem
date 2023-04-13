27e5b97a051b8a11dfffecf41017938227b25631
mahesh <gangurdemahesh9@gmail.com>
2023-04-13 23:32:10 +0530

    [mahesh]add greeting message

---
+++ Day15_Day16_ReflationPracticeProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15_Day16_ReflationPracticeProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Day 15 and Day 16 Practice Problem");
+
+        }
+    }
+}

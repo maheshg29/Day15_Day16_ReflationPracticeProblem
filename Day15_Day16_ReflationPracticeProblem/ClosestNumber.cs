9b9f1303eac4610ce879fb7eb132eb3b5949685c
mahesh <gangurdemahesh9@gmail.com>
2023-04-14 18:39:25 +0530

    [mahesh]add program file for find Closest Number with all even digit

--- Day15_Day16_ReflationPracticeProblem/ClosestNumber.cs
+++ Day15_Day16_ReflationPracticeProblem/ClosestNumber.cs
@@ -10,33 +10,64 @@ namespace Day15_Day16_ReflationPracticeProblem
     {
         public static int FindClosestEvenNumber(int givenNumber)
         {
-            int i = 0;
-            if (givenNumber == 0)
+            if (givenNumber > 0)
             {
-                return 2;
-            }
-            
-            for (i = givenNumber+1; i < givenNumber + 100; i++)
-            {
-                int[] digitArray = ClosestNumber.getDigit(i);
-                if (i % 2 == 0)
+                int i = 0;
+                if (givenNumber == 0)
                 {
-                    bool allOk=true;
-                    for (int j = 0; j < digitArray.Length; j++)
+                    return 2;
+                }
+
+                for (i = givenNumber + 1; i < givenNumber + 100; i++)
+                {
+                    int[] digitArray = ClosestNumber.getDigit(i);
+                    if (i % 2 == 0)
                     {
-                        if (digitArray[j] % 2 != 0)
+                        bool allOk = true;
+                        for (int j = 0; j < digitArray.Length; j++)
                         {
-                            allOk = false;
-                            break;
+                            if (digitArray[j] % 2 != 0)
+                            {
+                                allOk = false;
+                                break;
+                            }
+                        }
+                        if (allOk)
+                        {
+                            return i;
                         }
                     }
-                    if (allOk)
-                    {
-                        return i;
-                    }
                 }
+                return 0;
             }
-            return 0;
+            else 
+            {
+                for (int i = givenNumber + 1; i < givenNumber + 100; i++)
+                {
+                    i = Math.Abs(i); // for remove -ve sing
+                    int[] digitArray = ClosestNumber.getDigit(i);
+                    if (i % 2 == 0)
+                    {
+                        bool allOk = true;
+                        for (int j = 0; j < digitArray.Length; j++)
+                        {
+                            if (digitArray[j] % 2 != 0)
+                            {
+                                allOk = false;
+                                break;
+                            }
+                        }
+                        
+                        if (allOk)
+                        {
+                            i *= -1;
+                            return i;
+                        }
+                    }
+                    i *= -1; // add -ve sing
+                }
+                return 0;
+            }
         }
         public static int[] getDigit(int number)
         {
